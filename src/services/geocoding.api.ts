727d463f3b2c19a244921f0dca5343db15814753
CrawFish666 <emelyakoro4@mail.ru>
2026-08-14 19:33:23 +0400

    feat: Добавлен поиск городов по имени через Geocoding API

--- src/services/geocoding.api.ts
+++ src/services/geocoding.api.ts
@@ -1,8 +1,11 @@
 import type {
-	City
+	City,
+	GeocodingApiResponse,
+	GeocodingApiResult,
+	GeocodingApiError
 } from "../types/geocoding"
 
-
+const GEOCODE_API_URL = import.meta.env.VITE_GEOCODE_API_URL;
 const REVERSE_GEOCODE_MAPS_API_URL = import.meta.env.VITE_REVERSE_GEOCODE_MAPS_API_URL;
 const REVERSE_GEOCODE_MAPS_API_KEY = import.meta.env.VITE_REVERSE_GEOCODE_MAPS_API_KEY;
 
@@ -55,3 +58,63 @@ export async function getCityByCoordinates(latitude: number, longitude: number, 
 	}
 
 }
+
+function mapCity(result: GeocodingApiResult): City {
+	return {
+		id: result.id,
+		name: result.name,
+		latitude: result.latitude,
+		longitude: result.longitude,
+		country: result.country,
+		countryCode: result.country_code.toUpperCase(),
+		state: result.admin1
+	};
+}
+
+export async function searchCities({
+	query,
+	count = 5,
+	language = "ru",
+	countryCode,
+	signal
+}: SearchCitiesParams): Promise<City[]> {
+
+	// Некая защита от пробелов и если длина <2 символов просто ничего не делаем
+	const trimmedQuery = query.trim();
+
+	if (trimmedQuery.length < 2) return [];
+
+	const url = new URL(GEOCODE_API_URL);
+
+	// Преобразуем параметры в объект
+	const params = new URLSearchParams({
+		name: trimmedQuery,
+		count: String(count),
+		language,
+	})
+
+	// Если пользак передает countryCode, то пихаем в объект параметров
+	if (countryCode) {
+		params.set("countryCode", countryCode);
+	}
+
+	// Преобразуем объект параметров в строку и пихаем в объект для поиска
+	url.search = params.toString();
+
+	const response = await fetch(url, {
+		signal
+	});
+
+	if (!response.ok) {
+		const errorData: GeocodingApiError = await response.json();
+
+		throw new Error(errorData.reason);
+	}
+
+	// Ожидаем ответ по интерфейсу
+	const data: GeocodingApiResponse = await response.json();
+
+	// Преобразуем ответ в нужные нам данные, либо в пустоту.
+	return data.results?.map(mapCity) ?? [];
+}
+
